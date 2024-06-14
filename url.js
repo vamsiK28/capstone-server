00d066d7124d284ee9a6e156121fc27ba7970fdf
K Vamsi Krishna <vamsishreya0308@gmail.com>
2024-06-14 10:42:38 +0530

    updated env variabels

--- url.js
+++ url.js
@@ -1,2 +1,4 @@
-let url = "mongodb+srv://admin:admin@cluster0.vuq1vfq.mongodb.net/capstone";
+const dbUsername = process.env.dbUsername;
+const dbPassword = process.env.dbPassword;
+let url = `mongodb+srv://${admin}:${admin}@cluster0.vuq1vfq.mongodb.net/capstone`;
 module.exports = url;
