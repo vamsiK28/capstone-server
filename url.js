868a57f1d3b17f09dab24c387b103be3d3031b68
K Vamsi Krishna <vamsishreya0308@gmail.com>
2024-06-14 10:47:03 +0530

    changed my stupid mistake

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
 const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
-let url = `mongodb+srv://${admin}:${admin}@cluster0.vuq1vfq.mongodb.net/capstone`;
+let url = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.vuq1vfq.mongodb.net/capstone`;
 module.exports = url;
